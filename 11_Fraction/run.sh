cc30696426f3c6f1e4de1a2a5a081368fb4e44a6
Jonathan Stahl <jonathan.stahl@uzh.ch>
2018-10-14 21:04:16 +0200

    update run script

--- 11_Fraction/run.sh
+++ 11_Fraction/run.sh
@@ -2,6 +2,9 @@
 
 TARGET="11_Fraction"
 
+# To setup run once:
+# mkdir build && cd build && cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+
 # Build executable and exit if error occurs
 cmake --build build/ --target $TARGET
 retValue=$?
@@ -9,8 +12,8 @@ if [ $retValue -ne 0 ];  then
 	exit 1;
 fi
 
-# Update compile commands (cmake command: -DCMAKE_EXPORT_COMPILE_COMMANDS=ON)
+# Update compile commands (install compdb: pip install compdb)
 compdb -p build/ list > compile_commands.json
 
 # Run
-build/$TARGET -i
+build/$TARGET
--- 41_Traits/run.sh
+++ 41_Traits/run.sh
@@ -2,6 +2,9 @@
 
 TARGET="41_Traits"
 
+# To setup run once:
+# mkdir build && cd build && cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+
 # Build executable and exit if error occurs
 cmake --build build/ --target $TARGET
 retValue=$?
@@ -9,7 +12,7 @@ if [ $retValue -ne 0 ];  then
 	exit 1;
 fi
 
-# Update compile commands (cmake command: -DCMAKE_EXPORT_COMPILE_COMMANDS=ON)
+# Update compile commands (install compdb: pip install compdb)
 compdb -p build/ list > compile_commands.json
 
 # Run
