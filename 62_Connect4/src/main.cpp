b14e59781ed267e187a3283f1602728f248221c7
Jonathan Stahl <jonathan.stahl@uzh.ch>
2018-11-02 11:48:04 +0100

    final fix

--- 62_Connect4/src/main.cpp
+++ 62_Connect4/src/main.cpp
@@ -11,21 +11,26 @@ using playfieldtraits = playfield_traits<playfield>;
 
 int main(int argc, char const *argv[]) {
   playfield field;
+
+  // Player 1
   player<playfield> player1(1);
+  aiplayer<playfield> aiplayer1(1);
+  alphonse::player<playfield> alphie1(1);
+  randomplayer<playfield> randplayer1(1);
+
+  // Player 2
   player<playfield> player2(2);
-  randomplayer<playfield> randplayer2(2);
-
-  // AI
   aiplayer<playfield> aiplayer2(2);
-  alphonse::player<playfield> alphie(1);
-
+  randomplayer<playfield> randplayer2(2);
+  alphonse::player<playfield> alphie2(2);
 
   field.print();
 
   for (;;) {
     cout << "Player 1" << endl;
-    field.insertstone(player1.play(field), player1.getplayerid());
-    // field.insertstone(alphie.play(field), 1);
+    // field.insertstone(player1.play(field), player1.getplayerid());
+    field.insertstone(aiplayer1.play(field), 1);
+    // field.insertstone(alphie1.play(field), 1);
     field.print();
     if (playfieldtraits::haswon(field, 1)) {
       cout << "Player 1 has won!" << endl;
@@ -33,8 +38,8 @@ int main(int argc, char const *argv[]) {
     }
 
     cout << "Player 2" << endl;
-    field.insertstone(player2.play(field), player2.getplayerid());
-    // field.insertstone(aiplayer2.play(field), 2);
+    // field.insertstone(player2.play(field), player2.getplayerid());
+    field.insertstone(aiplayer2.play(field), 2);
     // field.insertstone(randplayer2.play(field), player2.getplayerid());
     field.print();
     if (playfieldtraits::haswon(field, 2)) {
