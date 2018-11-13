c1d12a19e2e9a3c8799f10e0d54ef5e6b123832b
Jonathan Stahl <jonathan.stahl@uzh.ch>
2018-11-13 17:13:30 +0100

    renaming

---
+++ 31_Traits/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "Pvector.h"
+#include "Pset.h"
+
+using namespace std;
+
+void foo(Pvector<int> *pv) {
+    pv->push_back(pv->getSize());
+
+    for (int i = 0; i < pv->getSize(); i++) {
+        pv->printElement(i);
+    }
+}
+
+void bar(Pset<string> *ps) {
+	ps->insert_back("zookie" + to_string(ps->getSize()));
+
+	ps->printSet();
+}
+
+int main() {
+	cout << "----- Pvector -----" << endl;
+
+    Pvector<int> pv("tmp/pvector-int.txt");
+    foo(&pv);
+
+    cout << "----- Pset -----" << endl;
+
+    Pset<string> ps("tmp/pset-string.txt");
+    bar(&ps);
+
+    return 0;
+}
